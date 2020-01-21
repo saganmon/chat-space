b1a695cdf0e5e865d50151a868d411e4d291c338
saganmon <fevers24@gmail.com>
2020-01-21 17:34:59 +0900

    Update rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,5 +65,5 @@ RSpec.configure do |config|
   Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include ControllerMacros, type: :controller
-  
+
 end
